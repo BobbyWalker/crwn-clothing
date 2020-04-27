935279346181419b5d24a22b5b13073e812d4317
Bobby Walker <BobbyWalker@users.noreply.github.com>
2020-04-26 22:39:14 -0500

    Added back end server

--- client/src/components/stripe-button/StripeButton.comonent.jsx
+++ client/src/components/stripe-button/StripeButton.comonent.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
 const StripeButton = ({price}) => {
     const priceForStripe = price * 100,
         publishableKey = 'pk_test_RBRyqJB9b7S6TLXQzf4TC98r00MF11TPmG'
 
     const onToken = token => {
-        console.log(token)
-        alert('Payment Successfull')
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        })
+            .then(res => {
+                alert('Payment successful')
+            })
+            .catch(err => {
+                console.log('Payment error: ', err)
+                alert('There was an issue with your payment.  Please use the provided card information')
+            })
     }
 
     return (
